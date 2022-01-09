84d07114ee0dda328a183f3b2ef375f4f0b0c1d8
Corbin Crutchley <crutchcorn@gmail.com>
2022-01-08 18:52:08 -0800

    remove unused variables

--- authorize.ts
+++ authorize.ts
@@ -4,11 +4,9 @@ import * as queryString from "query-string";
 /**
  * GH Env Vars
  */
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CLIENT_ID = process.env.CLIENT_ID;
 const SCOPE_STR = process.env.SCOPE_STR;
 const SERVER_CALLBACK_URL_BASE = process.env.SERVER_CALLBACK_URL_BASE;
-const FINAL_CALLBACK_URL_BASE = process.env.FINAL_CALLBACK_URL_BASE;
 
 export default (_req: VercelRequest, res: VercelResponse) => {
     const state = uuid();
--- callback.ts
+++ callback.ts
@@ -7,8 +7,6 @@ import axios from "axios";
  */
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CLIENT_ID = process.env.CLIENT_ID;
-const SCOPE_STR = process.env.SCOPE_STR;
-const SERVER_CALLBACK_URL_BASE = process.env.SERVER_CALLBACK_URL_BASE;
 const FINAL_CALLBACK_URL_BASE = process.env.FINAL_CALLBACK_URL_BASE;
 
 export default async (_req: VercelRequest, res: VercelResponse) => {
